bb095c1f7bb9375dace7eded8fbeacfe1dabca52
laura <lauratineo25@gmail.com>
2025-06-11 19:03:12 -0400

    Add modal close button functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
-const editProfileForm = editProfileModal.querySelector(".edit-profile__form");
+const profileForm = document.forms[""profile-form""];
 const editProfileNameInput = editProfileModal.querySelector("#profile-name-input");
 const editProfileDescriptionInput = editProfileModal.querySelector("#profile-description-input");
 
@@ -15,14 +15,29 @@ const profileDescriptionEl = document.querySelector(".profile__description");
 editProfileBtn.addEventListener("click", function () {
     editProfileNameInput.value = profileNameEl.textContent;
     editProfileDescriptionInput.value = profileDescriptionEl.textContent;
-    editProfileModal.classList.add("modal_is-opened");  });
-
+    openModal(editProfileModal); function openModal(modal) {
+        modal.classList.add("modal_is-opened");
+      }
+      
+      function closeModal(modal) {
+        modal.classList.remove("modal_is-opened");
+      }
+  });
 editProfileCloseBtn.addEventListener("click", function () {
     editProfileModal.classList.remove ("modal_is-opened"); }); 
 
+    const newPostForm = newPostModal.querySelector(".new-post__form");
+    const cardImageInput = newPostModal.querySelector("#card-image-input");
+    const cardTitleInput = newPostModal.querySelector("#card-title-input");
 
+    function handleNewPostFormSubmit(evt) {
+        evt.preventDefault();
+        console.log("Image link:", cardImageInput.value);
+        console.log("Title:", cardTitleInput.value);
+        closeModal(newPostModal);
+    }
 newPostBtn.addEventListener("click", function () {
-    newPostModal.classList.add ("modal_is-opened"); });
+     newPostModal.classList.add ("modal_is-opened"); });
 
 
 newPostCloseBtn.addEventListener("click", function () {
